c3b16272b4a3f931e70c8e72cf7e48db96e6d969
Krionee <kdima3069@gmail.com>
2024-11-21 14:13:18 +0300

    pupupu

--- keyboards/functionsInlineKeyboard.go
+++ keyboards/functionsInlineKeyboard.go
@@ -1,6 +1,11 @@
 package keyboards
 
-import tu "github.com/mymmrac/telego/telegoutil"
+import (
+	"itproj/models"
+
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
 
 var FunctionsInlineKeyboard = tu.InlineKeyboard(
 	tu.InlineKeyboardRow(
@@ -9,5 +14,25 @@ var FunctionsInlineKeyboard = tu.InlineKeyboard(
 	),
 	tu.InlineKeyboardRow(
 		tu.InlineKeyboardButton("Показать все мои задания").WithCallbackData("showAllTasks"),
+		tu.InlineKeyboardButton("Выдать таску").WithCallbackData("createTask"),
 	),
 )
+
+func CreateInlineKeyboard(lst []models.Group) telego.InlineKeyboardMarkup {
+
+	rows := make([][]telego.InlineKeyboardButton, 0, 1)
+	count := len(lst) / 2
+	for i := 0; i < count; i += 2 {
+		var row []telego.InlineKeyboardButton
+		for j := i; (j < i+2) && j < len(lst); j++ {
+			row = append(row, telego.InlineKeyboardButton{
+				Text:         lst[j].Name,
+				CallbackData: lst[j].ID.Hex(),
+			})
+		}
+		rows = append(rows, row)
+	}
+	return telego.InlineKeyboardMarkup{
+		InlineKeyboard: rows,
+	}
+}
